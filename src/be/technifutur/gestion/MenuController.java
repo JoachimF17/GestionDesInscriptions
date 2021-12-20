8aacf40c08af26ae320d9fb413b47084806ac922
Joachim <joachim.frances@hotmail.be>
2021-12-20 09:25:56 +0100

    Implémentation view/controller et affichage d'un menu

---
+++ src/be/technifutur/gestion/MenuController.java
@@ -0,0 +1,12 @@
+package be.technifutur.gestion;
+
+public class MenuController
+{
+    //attributs
+    private MenuView vue;
+    //methodes
+    public void AfficheMenuPrincipal()
+    {
+        this.vue.MenuPrincipal();
+    }
+}
---
+++ src/be/technifutur/gestion/MenuView.java
@@ -0,0 +1,12 @@
+package be.technifutur.gestion;
+
+public class MenuView
+{
+    public void MenuPrincipal()
+    {
+        System.out.println("Menu principal");
+        System.out.println("--------------");
+        System.out.println("1. Gerer activite");
+        System.out.println("9. Quitter");
+    }
+}

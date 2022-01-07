ae99a0bf91992d5b739a5db22913b0b7966ccd50
JoachimF17 <joachim.frances@hotmail.be>
2022-01-07 15:57:14 +0100

    Fin (?) schedule update

--- src/be/technifutur/gestion/schedule/ScheduleUpdate.java
+++ src/be/technifutur/gestion/schedule/ScheduleUpdate.java
@@ -1,6 +1,7 @@
 package be.technifutur.gestion.schedule;
 
 import be.technifutur.gestion.activity.ActivityRead;
+import be.technifutur.gestion.activity.ActivityType;
 import be.technifutur.gestion.activity.ActivityView;
 import be.technifutur.gestion.activity.ListActivityType;
 
@@ -24,12 +25,16 @@ public class ScheduleUpdate implements Callable
         this.listActivityType = listActivityType;
     }
 
-    @Override public Object call()
+    @Override
+    public Object call()
     {
+        //variables
         boolean inputInvalide = true;
+        //objets
         String input = "";
         LocalDateTime start;
         LocalDateTime end;
+        ActivityType activityType;
         String oldName;
         String newName;
         String regex = "([4-9]|[1-2][0-9]?|3[0-1]?|0[1-9])/([2-9]|1[0-2]?|0[1-9])/([1-9][0-9]{3}) ([0-1][0-9]|2[0-3]):([0-5][0-9])";
@@ -38,40 +43,79 @@ public class ScheduleUpdate implements Callable
 
         oldName = vue.getUpdateActivityName();
 
-        while(inputInvalide)
+        while (inputInvalide)
         {
             input = vue.getUpdateNewName();
 
-            if(liste.getActivity(input) == null || liste.getActivity(input).equals(liste.getActivity(oldName)))
+            if (liste.getActivity(input) == null || liste.getActivity(input).equals(liste.getActivity(oldName)))
                 inputInvalide = false;
             else
                 vue.setError("ce nom existe deja pour une autre activite");
         }
 
-        if(input.isEmpty())
+        if (input.isEmpty())
             newName = oldName;
         else
             newName = input;
 
         vue.setError(null);
-        inputInvalide = true;
 
-        /*while(inputInvalide)
+        activityRead.call();
+        input = vue.getUpdateNewActivityType();
+
+        if (input.isEmpty() || listActivityType.get(input).equals(liste.getActivity(oldName).getActivityType()))
+            activityType = liste.getActivity(oldName).getActivityType();
+        else if(listActivityType.get(input) != null)
+            activityType = listActivityType.get(input);
+        else
         {
-            activityRead.call();
-            input = vue.getUpdateNewActivityType();
+            String confirmation;
+            boolean inputActiviteInvalide = true;
 
-            if(listActivityType.get(input) == null)
+            while(inputActiviteInvalide)
+            {
+                confirmation = vue.nonExistentActivityType().toLowerCase();
+
+                if (confirmation.isEmpty() || confirmation.charAt(0) == 'n')
+                    inputActiviteInvalide = false;
+                else if (confirmation.charAt(0) == 'o')
+                {
+                    boolean inputInscriptionInvalide = true;
+                    String tempRegistration = "";
+                    ActivityView activityView = new ActivityView();
+
+                    while(inputInscriptionInvalide)
+                    {
+                        tempRegistration = activityView.createActivityRegistration();
+
+                        if(tempRegistration.isEmpty() || tempRegistration.toLowerCase().charAt(0) == 'n')
+                        {
+                            inputInscriptionInvalide = false;
+                            listActivityType.addActivityType(input, false);
+                        }
+                        else if(tempRegistration.toLowerCase().charAt(0) == 'o')
+                        {
+                            inputInscriptionInvalide = false;
+                            listActivityType.addActivityType(input, true);
+                        }else
+                            this.vue.setError("entrez (o) ou (n)");
+                    }
+                    inputActiviteInvalide = false;
+                } else
+                    vue.setError("entrez (o) ou (n)");
+            }
+
+            activityType = listActivityType.get(input);
         }
 
         vue.setError(null);
-        inputInvalide = true;*/
+        inputInvalide = true;
 
-        while(inputInvalide)
+        while (inputInvalide)
         {
             input = vue.updateDateTimeDebut();
 
-            if(input.isEmpty() || input.matches(regex))
+            if (input.isEmpty() || input.matches(regex))
                 inputInvalide = false;
             else
                 vue.setError("format invalide, merci de respecter le format specifie (format : J/M/AAAA HH:MM)");
@@ -80,16 +124,16 @@ public class ScheduleUpdate implements Callable
         vue.setError(null);
         inputInvalide = true;
 
-        if(input.isEmpty())
+        if (input.isEmpty())
             start = liste.getActivity(oldName).getStartTime();
         else
             start = LocalDateTime.parse(input, formatter);
 
-        while(inputInvalide)
+        while (inputInvalide)
         {
             input = vue.updateDateTimeFin();
 
-            if(input.isEmpty() || input.matches(regex))
+            if (input.isEmpty() || input.matches(regex))
                 inputInvalide = false;
             else
                 vue.setError("format invalide, merci de respecter le format specifie (format : J/M/AAAA HH:MM)");
@@ -97,10 +141,13 @@ public class ScheduleUpdate implements Callable
 
         vue.setError(null);
 
-        if(input.isEmpty())
-            start = liste.getActivity(oldName).getEndTime();
+        if (input.isEmpty())
+            end = liste.getActivity(oldName).getEndTime();
         else
-            start = LocalDateTime.parse(input, formatter);
+            end = LocalDateTime.parse(input, formatter);
+
+        liste.activities.remove(oldName.toUpperCase());
+        liste.addActivity(start, end, newName, activityType);
 
         return null;
     }
